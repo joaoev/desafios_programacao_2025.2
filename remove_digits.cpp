0364ffcd612f9759379098a749879efd0a79c32d
joaoev <joaoalvesxp@alu.ufc.br>
2025-11-28 01:18:42 -0300

    feat: resolve book and digits

--- remove_digits.cpp
+++ remove_digits.cpp
@@ -1,44 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define MAXN 1000000 + 1
+int dp[MAXN];
+
+vector<int> coletaDigitos(int x)
+{
+
+    cout << "Collecting digits for x = " << x << endl;
+    vector<int> dig;
+    int aux = x;
+
+    cout << "Auxiliary value before collecting digits: " << aux << endl;
+
+    while (aux > 0)
+    {
+        dig.push_back(aux % 10);
+        aux = aux / 10;
+    }
+
+    return dig;
+}
+
+int solve(int x)
+{
+    cout << "Solving for x = " << x << endl;
+    if (x == 0)
+        return 0;
+    if (x <= 9)
+        return 1;
+
+    if (dp[x] != -1)
+    {
+        cout << "Memoized result for x = " << x << " is " << dp[x] << endl;
+        return dp[x];
+    }
+
+    int resultado = MAXN + 128;
+
+    for (int d : coletaDigitos(x))
+    {
+        if (d != 0)
+        {
+            resultado = min(resultado, 1 + solve(x - d));
+        }
+    }
+
+    dp[x] = resultado;
+
+    return resultado;
+}
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
     int n;
     cin >> n;
 
-    const int INF = 1000000000;
-
-    vector<int> dp(n + 1, INF);
-
-    dp[0] = 0;
-
-    for (int x = 1; x <= n; ++x)
+    for (int i = 0; i < n; i++)
     {
-        int y = x;
-
-        cout << "Processing number: " << x << endl;
-        cout << "Current dp state: " << dp[y] << endl;
-
-        while (y > 0)
-        {
-            int d = y % 10;
-
-            cout << "  Considering digit: " << d << endl;
-            y = y / 10;
-            cout << "  Remaining number after removing digit: " << y << endl;
-
-            if (d > 0 && x - d >= 0)
-            {
-                dp[x] = min(dp[x], 1 + dp[x - d]);
-                cout << "  Updated dp[" << x << "] to " << dp[x] << endl;
-            }
-        }
+        dp[i] = -1;
     }
 
-    cout << dp[n] << endl;
+    cout << solve(n) << endl;
 
     return 0;
 }
